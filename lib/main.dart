26372f1ec1e71760311697299e5d0c491947e234
khlood-ahmed <khlood.ahmed688@gmail.com>
2020-11-17 21:01:12 +0200

    add image folder and icon for gp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gp_project/routes/Home.dart';
-import 'package:gp_project/routes/login_screen.dart';
-import 'package:gp_project/routes/signup_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,14 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       home: Home(),
-
-      initialRoute: LoginScreen.id,
-      routes:{
-        LoginScreen.id:(context)=>LoginScreen(),
-        signup_screen.id:(context)=>signup_screen()
-      } ,
     );
   }
-}
+}    
